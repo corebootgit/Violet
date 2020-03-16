a530da0f1fcb9fc17db6aa2381e6c29f8e2c5d50
Marcin <coreboot@tlen.pl>
2020-03-17 00:55:29 +0100

    scaled viewport, tiled map

---
+++ android/assets/graphics/kremek.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="kremek" tilewidth="128" tileheight="128" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="128" height="128" source="wall128.jpg"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="grass128.jpg"/>
+ </tile>
+</tileset>
--- core/src/com/chorsstudio/game/MyGame.java
+++ core/src/com/chorsstudio/game/MyGame.java
@@ -7,6 +7,11 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -14,6 +19,8 @@ public class MyGame extends ApplicationAdapter {
 
 	public int WIDTH;
 	public int HEIGHT;
+	public int SCREEN_WIDTH;
+	public int SCREEN_HEIGHT;
 
 	SpriteBatch batch;
 	Texture img;
@@ -21,41 +28,74 @@ public class MyGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private StretchViewport viewport;
 
-
+	TiledMap tiledMap;
+	TiledMapRenderer tiledMapRenderer;
+	private int xpos;
+	private int ypos;
+	private int xdelta = 8;
+	private int ydelta = 8;
 
 	@Override
 	public void create () {
 
-		WIDTH = Gdx.graphics.getWidth();
-		HEIGHT = Gdx.graphics.getHeight();
+		SCREEN_WIDTH = Gdx.graphics.getWidth();
+		SCREEN_HEIGHT = Gdx.graphics.getHeight();
+		WIDTH = 8*128;
+		HEIGHT = 14*128;
 
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("dvd.png");
 
 		camera = new OrthographicCamera();
 		viewport = new StretchViewport(WIDTH, HEIGHT, camera);
-		viewport.apply();
+
+		camera.update();
 		camera.position.set(WIDTH/2, HEIGHT/2, 0);
+
+		tiledMap = new TmxMapLoader().load("maps/kremek_math.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+		viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		viewport.apply();
 
 	}
 
 	@Override
 	public void render () {
-		camera.update();
 
-		camera.position.set(WIDTH/2, HEIGHT/2, 0);
-
-
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0.6f, 0.9f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch.begin();
+
+
+
+		camera.update();
+
+	//	camera.position.set(WIDTH/2, HEIGHT/2, 0);
 		batch.setProjectionMatrix(camera.combined);
-		batch.draw(img, WIDTH/2, HEIGHT/2);
-		batch.draw(img, 0, 0);
+
+		batch.begin();
+		batch.draw(img, xpos, ypos, 300, 300);
 		batch.end();
+
+
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+
+ 		if ( (xpos > (WIDTH - 300)) || (xpos < 0) ) xdelta = -xdelta;
+		if ( (ypos > (HEIGHT - 240)) || (ypos < -60) ) ydelta = -ydelta;
+		xpos += xdelta;
+		ypos += ydelta;
+
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			//camera.unproject(touchPos);
+			xpos = (int) touchPos.x - 150;
+			ypos = HEIGHT - (int) touchPos.y - 150;
+		}
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
